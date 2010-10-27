a2b2045c59d516ba3deebcc42256f41037700a25
Jim Safley <jsafley@gmu.edu>
2010-10-26 21:21:56 -0400

    Adds a prototype map using Google Maps API and uses a KML document to place markers.

--- kml.php
+++ kml.php
@@ -4,4 +4,4 @@ require 'config.php';
 // Write KML process.
 require_once 'Plants/Process/Kml.php';
 $kml = new Plants_Process_Kml($db);
-$kml->write(1, 1000, true);
+$kml->write($_REQUEST['searchId'], 500, true);
---
+++ map.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<style type="text/css">
+  html { height: 100% }
+  body { height: 100%; margin: 0px; padding: 0px }
+  #map_canvas { height: 100% }
+</style>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+var kml;
+var map;
+var markers = [];
+
+function initialize() {
+    
+    // Set the map.
+    var latlng = new google.maps.LatLng(0, 0);
+    var myOptions = {
+        zoom: 2,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+    
+    // Get the KML and set the XML object.
+    $.post("http://localhost/Plants/kml.php", {searchId: 1}, function (data) {
+        kml = data;
+        mapKml();
+    });
+}
+
+// http://code.google.com/apis/maps/documentation/javascript/overlays.html#RemovingOverlays
+function addMarker(point) {
+    marker = new google.maps.Marker({
+        position: point,
+        map: map
+    });
+    markers.push(marker);
+}
+// Deletes all markers in the array by removing references to them
+function deleteMarkers() {
+    if (markers) {
+        for (i in markers) {
+            markers[i].setMap(null);
+        }
+        markers.length = 0;
+    }
+}
+
+// http://articles.sitepoint.com/article/google-maps-api-jquery
+function mapKml() {
+    deleteMarkers();
+    $(kml).find("Placemark").each(function () {
+        var name = $(this).children("name").text();
+        var description = $(this).children("description").text();
+        var coordinates = $(this).find("coordinates").text().split(",");
+        var point = new google.maps.LatLng(coordinates[1], coordinates[0]);
+        addMarker(point);
+    });
+}
+
+// http://api.jquery.com/category/traversing/tree-traversal/
+function parseKml() {
+    $(kml).find("Placemark").each(function () {
+        var name = $(this).children("name").text();
+        var description = $(this).children("description").text();
+        $("#kml").append($("<a>", {href: description, text: name}));
+        $("#kml").append('<table>');
+        $(this).find("Data").each(function () {
+            var dataName = $(this).attr("name");
+            var dataDisplayName = $(this).children("displayName").text();
+            var dataValue = $(this).children("value").text()
+            $("#kml").append('<tr><td>' + dataDisplayName + '</td><td>' + dataValue + '</td></tr>');
+        })
+        $("#kml").append('</table>');
+    });
+}
+
+// Identical herbariums are named differently for every resource. May have to 
+// parse out formal name of herbarium during ingest.
+function getHerbariums() {
+    // http://www.hunlock.com/blogs/Mastering_Javascript_Arrays
+    var herbariums = [];
+    // http://api.jquery.com/category/selectors/
+    $(kml).find("Data[name='herbarium']").each(function () {
+        var value = $(this).children('value').text();
+        if ($.inArray(value, herbariums) == -1) {
+            herbariums.push(value);
+        }
+    });
+    alert(herbariums);
+}
+
+function getCollectionYears() {
+    var collectionYears = [];
+    $(kml).find("Data[name='collection_year']").each(function () {
+        var value = $(this).children('value').text();
+        if ($.inArray(value, collectionYears) == -1 && value.length > 0) {
+            collectionYears.push(value);
+        }
+    });
+    $.each(collectionYears.sort(), function (index, value) {
+        $("#content-window").append($("<a>", {
+            onclick: "mapCollectionYears(" + value + ")", 
+            text: value
+        }));
+        $("#content-window").append($("<br />"));
+    });
+}
+
+function mapCollectionYears(value) {
+    deleteMarkers();
+    $(kml).find("Placemark:has(Data[name='collection_year']):has(value):contains(" + value + ")").each(function () {
+        var coordinates = $(this).find("coordinates").text().split(",");
+        var point = new google.maps.LatLng(coordinates[1], coordinates[0]);
+        addMarker(point);
+    });
+}
+</script>
+</head>
+<body onload="initialize()">
+    <div id="map" style="width:79%; height:100%; float:left"></div>
+    <div id="content-window" style="width:19%; height:100%; float:left">
+        <button onclick="alert(kml);">Check for KML</button>
+        <button onclick="mapParse();">Parse KML</button>
+        <button onclick="mapKml();">Map KML</button>
+        <button onclick="getHerbariums();">Get Herbariums</button>
+        <button onclick="getCollectionYears();">Get Collection Years</button>
+        <button onclick="deleteMarkers();">Delete Markers</button>
+    </div>
+    <!--<div id='kml'></div>-->
+</body>
